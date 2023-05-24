40eb64c1c956fabd61fdc3b1a4e94bd73761e3fa
kipkirui63 <kiprutonyachire@gmail.com>
2023-05-24 12:56:56 +0300

    I added a placeholder to CSS/index.css

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+/**contactUs */
+.text-with-line {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .line {
+    width: 100%;
+    height: 2px;
+    background-color: black;
+    margin-top: -5px;
+  }
+  
+  input {
+    margin-top: 5px;
+    border: none;
+    background: none;
+    text-align: center;
+    font-size: inherit;
+  }
+  
