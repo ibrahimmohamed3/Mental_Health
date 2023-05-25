10bb3ece769fa8a24cc6493a36339ea7986ed795
kipkirui63 <kiprutonyachire@gmail.com>
2023-05-25 16:22:40 +0300

    I updated to check valid email

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,3 @@
-/**contactUs */
 const nameInput = document.getElementById('name-input');
 const emailInput = document.getElementById('email-input');
 const messageInput = document.getElementById('message-input');
@@ -11,13 +10,36 @@ nameInput.addEventListener('input', function() {
   }
 });
 
+emailInput.addEventListener('input', function() {
+  if (isValidEmail(emailInput.value)) {
+    emailInput.style.backgroundColor = 'pink';
+  } else {
+    emailInput.style.backgroundColor = 'transparent';
+  }
+});
+
+messageInput.addEventListener('input', function() {
+  if (messageInput.value !== '') {
+    messageInput.style.backgroundColor = 'pink';
+  } else {
+    messageInput.style.backgroundColor = 'transparent';
+  }
+});
+
 document.getElementById('send-button').addEventListener('click', function() {
-  // Retrieve input values
   var name = nameInput.value;
   var email = emailInput.value;
   var message = messageInput.value;
 
-  alert("Message sent successfully!");
+  if (name !== '' && isValidEmail(email) && message !== '') {
+    alert("Message sent successfully!");
+  } else {
+    alert("Please fill in all the fields correctly.");
+  }
 });
 
-
+function isValidEmail(email) {
+  // Email validation regular expression
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
